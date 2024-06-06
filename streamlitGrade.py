43bb1eee534c8ef6f0668e407abe849206419f34
COMEWLECOME <MOHASOFT@GMAIL.com>
2024-06-06 08:21:06 +0200

    final de chez final

--- streamlitGrade.py
+++ streamlitGrade.py
@@ -41,49 +41,46 @@ def calculate_age(X):
     return current_year - X
 
 def main():
-    st.title("Prédiction de notes")
+    st.title("Prédiction du Grade")
 
-    # Sidebar pour charger le modèle
-    st.sidebar.title("Charger le modèle")
-    model_path = st.sidebar.file_uploader("Sélectionnez le modèle à charger", type="joblib")
-    if model_path:
-        model = load_model(model_path)
-        st.sidebar.success("Modèle chargé avec succès!")
+    # Charger le modèle directement
+    model_path = "RandomForestRegressor_model.joblib"
+    model = load_model(model_path)
 
-        # Obtenir les suggestions pour chaque attribut (une seule fois)
-        courses = get_suggestions_from_db("course")
-        bodyemotions = get_suggestions_from_db("bodyemotion")
-        countries = get_suggestions_from_db("country")
-        genders = get_suggestions_from_db("gender")
-        csps = get_suggestions_from_db("csp")
-        educations = get_suggestions_from_db("level_of_education")
+    # Obtenir les suggestions pour chaque attribut (une seule fois)
+    courses = get_suggestions_from_db("course")
+    bodyemotions = get_suggestions_from_db("bodyemotion")
+    countries = get_suggestions_from_db("country")
+    genders = get_suggestions_from_db("gender")
+    csps = get_suggestions_from_db("csp")
+    educations = get_suggestions_from_db("level_of_education")
 
-        # Interface utilisateur pour saisir les informations nécessaires pour la prédiction
-        st.header("Entrez les informations pour la prédiction")
-        
-        # Champs pour la saisie des informations
-        course = st.selectbox("Cours", courses)
-        bodyemotion = st.selectbox("Émotion du corps", bodyemotions)
-        country = st.selectbox("Pays", countries)
-        gender = st.selectbox("Genre", genders)
-        year_of_birth = st.number_input("Année de naissance", min_value=1900, max_value=2024, value=2000)
-        csp = st.selectbox("CSP", csps)
-        level_of_education = st.selectbox("Niveau d'éducation", educations)
+    # Interface utilisateur pour saisir les informations nécessaires pour la prédiction
+    st.header("Entrez les informations pour la prédiction")
+    
+    # Champs pour la saisie des informations
+    course = st.selectbox("Cours", courses)
+    bodyemotion = st.selectbox("Émotion du corps", bodyemotions)
+    country = st.selectbox("Pays", countries)
+    gender = st.selectbox("Genre", genders)
+    year_of_birth = st.number_input("Année de naissance", min_value=1900, max_value=2024, value=2000)
+    csp = st.selectbox("CSP", csps)
+    level_of_education = st.selectbox("Niveau d'éducation", educations)
 
-        # Préparer les données pour la prédiction
-        new_data = pd.DataFrame({
-            'course': [course],
-            'bodyemotion': [bodyemotion],
-            'country': [country],
-            'gender': [gender],
-            'year_of_birth': [year_of_birth],
-            'csp': [csp],
-            'level_of_education': [level_of_education]
-        })
+    # Préparer les données pour la prédiction
+    new_data = pd.DataFrame({
+        'course': [course],
+        'bodyemotion': [bodyemotion],
+        'country': [country],
+        'gender': [gender],
+        'year_of_birth': [year_of_birth],
+        'csp': [csp],
+        'level_of_education': [level_of_education]
+    })
 
-        if st.button("Prédire"):
-            prediction = predict_grade(model, new_data)
-            st.success(f"La prédiction de la note est : {prediction}")
+    if st.button("Prédire"):
+        prediction = predict_grade(model, new_data)
+        st.success(f"La prédiction de la note est : {prediction}")
 
 if __name__ == "__main__":
     main()
